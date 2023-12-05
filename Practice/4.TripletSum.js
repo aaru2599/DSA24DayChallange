f59ae17f905cac88fd709aa3740fea24e3529e42
aaru2599 <arvindmandre2@gmail.com>
2023-12-05 09:12:23 +0530

    Triplet sum

--- Practice/4.TripletSum.js
+++ Practice/4.TripletSum.js
@@ -4,16 +4,17 @@
  * @returns 
  * Program to Find a triplet that sum to a given value
  */
-const threeSum = (arr) => {
+const tripletSum = (arr) => {
     for (let i = 0; i < arr.length; i++) {
         for (let j = i + 1; j < arr.length; j++) {
             for (let k = j + 1; k < arr.length; k++) {
                 if (arr[i] + arr[j] + arr[k] === 24) {
-                    return [arr[i], arr[j], arr[k]]
+                    return `${arr[i]}, ${arr[j]}, ${arr[k]}`
                 }
             }
         }
     }
 }
 const array = [12, 3, 4, 1, 6, 9]
-console.log(threeSum(array));
+
+console.log(tripletSum(array));
