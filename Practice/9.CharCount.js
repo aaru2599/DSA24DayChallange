4b6fe5744949ad567c02d63f9eb19aed72cc38a1
aaru2599 <arvindmandre2@gmail.com>
2024-01-07 15:58:09 +0530

    Find Character

---
+++ Practice/9.CharCount.js
@@ -0,0 +1,11 @@
+const charCount=(input)=>{
+    let count=0
+for(let i=0;i<input.length;i++){
+    if(input[i]==="A"){
+        count++
+    }
+}
+return count
+}
+const str = "AAAABBBCCDAA"
+console.log(charCount(str));
