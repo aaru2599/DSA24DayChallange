d4f5e616369efff68fd87fa648e85680ef08d501
aaru2599 <arvindmandre2@gmail.com>
2024-01-16 00:31:31 +0530

    Unique occurance

--- Practice/15.UniqueOccurance.js
+++ Practice/15.UniqueOccurance.js
@@ -12,20 +12,39 @@
  */
 
 
-const uniqueOccurance = (arr) => {
-    let obj = {}
-    for (let i = 0; i < arr.length; i++) {
-        var key = arr[i]
+// const uniqueOccurance = (arr) => {
+//     let obj = {}
+//     for (let i = 0; i < arr.length; i++) {
+//         var key = arr[i]
 
+//         obj.hasOwnProperty(key) ?
+//             obj[key] = obj[key] + 1 :
+//             obj[key] = 1
+//     }
+//     // return obj
+//     const values = Object.values(obj)
+//     // console.log("values",values);
+//     const result = values.every((val, index) => values.indexOf(val) === index)
+//     return result
+// }
+// const arr = [1, 2]
+// console.log(uniqueOccurance(arr));
+
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var uniqueOccurrences = function (arr) {
+    let obj = {};
+    for (let key of arr) {
         obj.hasOwnProperty(key) ?
             obj[key] = obj[key] + 1 :
             obj[key] = 1
     }
-    // return obj
-    const values = Object.values(obj)
-    // console.log("values",values);
-    const result = values.every((val, index) => values.indexOf(val) === index)
-    return result
-}
-const arr = [1, 2]
-console.log(uniqueOccurance(arr));
+    const newArr = Object.values(obj);
+    const newSet = [...new Set(newArr)];
+    return newSet.length === newArr.length
+
+};
+arr = [-3, 0, 1, -3, 1, 1, 1, -3, 10, 0]
+console.log(uniqueOccurrences(arr));
