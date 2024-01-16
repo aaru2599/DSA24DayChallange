885ce91c1c4a9a36c52ea45036b07c9224a9e866
aaru2599 <arvindmandre2@gmail.com>
2024-01-16 23:37:56 +0530

    Reverse String

---
+++ Practice/18.reverseString.js
@@ -0,0 +1,9 @@
+const revString = (str) => {
+    let result = "";
+    for (let i = str.length-1 ; i >= 0; i--) {
+        result = result + str.charAt(i)
+    }
+    return result
+}
+const str = "My Name"
+console.log(revString(str));
