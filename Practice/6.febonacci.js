e0da43a54269acadaa44e9f2bbf707f3c2f50719
aaru2599 <arvindmandre2@gmail.com>
2024-01-23 23:05:32 +0530

    Febonnacci

--- Practice/6.febonacci.js
+++ Practice/6.febonacci.js
@@ -1,12 +1,14 @@
 const febonacci = () => {
     let a = 0;
     let b = 1;
+    let result=""
     for (let i = 0; i < 10; i++) {
-      console.log( a);
-        let temp = a + b;
-        a = b;
-        b = temp
+      let temp = a + b;
+      a = b;
+      b = temp
+      // console.log( a);
+        result=result+a+" "
     }
-    return a
+    return result
 }
 console.log(febonacci());
